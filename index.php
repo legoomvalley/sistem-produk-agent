6bd85f941e3973949c8c3f89cffd07e85a0a1899
AdinRC <93084433+AdinRC@users.noreply.github.com>
2022-06-29 16:46:25 +0800

    jadi kan login sebagai homepage

--- index.php
+++ index.php
@@ -1,29 +1,51 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="style.css" />
-    <title>HomePage</title>
-  </head>
-  <body>
-    <h2>Login Form</h2>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<br>
+<h2 style="text-align: center;">Login Form</h2>
 
-    <form action="/action_page.php" method="post">
-      <div class="container">
-        <label for="name"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="name" required />
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
-        <label for="pass"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="pass" required />
-
-        <button type="submit">Login</button>
-        <label> <input type="checkbox" checked="checked" name="remember" /> Remember me </label>
-      </div>
-
-      <div class="container" style="background-color: #f1f1f1">
-        <button type="button" class="cancelbtn">Cancel</button>
-        <span class="psw">Forgot <a href="#">password?</a></span>
-      </div>
-    </form>
-  </body>
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php" method="POST">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+    <div class="container">
+      <label for="uname"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="uname" required>
+      <label for="pass"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="pass" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+      <br><br>
+    Click here to
+    <a  href="register.php">Register</a>
+    </div>
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+</body>
 </html>
